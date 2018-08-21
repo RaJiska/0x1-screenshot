f90346b57662c02e8be3819ee2686644e799297f
Ra'Jiska <dodo.lasticot@gmail.com>
2018-08-21 17:47:38 +0200

    Initial Commit

---
+++ include/nnullptr-screenshot.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright	: Copyright (C) 2018 Doriann Corlouër. All rights reserved.
+ * File		: nnullptr-screenshot.h
+ * Author	: Doriann "Ra'Jiska" Corlouër
+ * Created	: Tue Aug 21st 16:31:52 2018
+ */
+
+#ifndef NNULLPTR_SCREENSHOT_H_
+#define NNULLPTR_SCREENSHOT_H_
+
+typedef struct
+{
+	int x;
+	int width;
+	int y;
+	int height;
+} area_coords_t;
+
+#endif /* NNULLPTR_SCREENSHOT_H_ */
---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright	: Copyright (C) 2018 Doriann Corlouër. All rights reserved.
+ * File		: main.c
+ * Author	: Doriann "Ra'Jiska" Corlouër
+ * Created	: Tue Aug 21st 16:10:35 2018
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char * const *argv)
+{
+	return EXIT_SUCCESS;
+}
